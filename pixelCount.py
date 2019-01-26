8eb6513051317dd662aeefdd6b5c04910b99c759
agosh-saini <agosh.saini@gmail.com>
2019-01-26 16:53:10 -0500

    File DNE fucnctionality

--- pixelCount.py
+++ pixelCount.py
@@ -1,21 +1,29 @@
 import numpy as np
 from PIL import Image
+import os
 
 def ratioBW(fileName):
-    #converting image to array
-    img = Image.open(fileName).convert('1')
-    imgArr = np.array(img);
+    #checking if file exists
+    if os.path.isfile('./'+ fileName) == True:
+        #converting image to array
+        img = Image.open(fileName).convert('1')
+        imgArr = np.array(img);
+        
+        #making it 1D
+        flatArr = imgArr.ravel()
+        
+        #calculating ratios
+        total = len(flatArr)
+        black = np.sum(flatArr)
+        white = total - black
+         
+        #Return [Black, White, Total]
+        return [black, white, total]    
+    else:
+        #returns if file Does not exist in directory
+        return False
     
-    #making it 1D
-    flatArr = imgArr.ravel()
-    
-    #calculating ratios
-    total = len(flatArr)
-    black = np.sum(flatArr)
-    white = total - black
-     
-    #Return [Black, White, Total]
-    return [black, white, total]
+
 
 #fileName = input('fileName: ')
 
