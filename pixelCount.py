5e2ee814fd79000c4cbc18e4b5db5064b90249af
agosh-saini <agosh.saini@gmail.com>
2019-01-26 21:21:24 -0500

    big ouf

--- pixelCount.py
+++ pixelCount.py
@@ -14,20 +14,47 @@ def ratioBW(fileName):
         
         #calculating ratios
         total = len(flatArr)
-        black = np.sum(flatArr)
-        white = total - black
+        white = np.sum(flatArr)
+        black = total - white
          
-        #Return [Black, White, Total]
-        return [black, white, total]    
+        #Return [white, black, total]
+        return [white, black, total]    
+    else:
+        #returns if file Does not exist in directory
+        return False
+    
+    
+def ratioBWImported(fileName):
+    #checking if file exists
+    if os.path.isfile('./'+ fileName) == True:
+        #converting image to array
+        img = Image.open(fileName).convert('1')
+        imgArr = np.array(img);
+        
+        #making it 1D
+        flatArr = imgArr.ravel()
+        
+        #calculating ratios
+        total = len(flatArr) - 53369
+        white = np.sum(flatArr) - 53369
+        black = total - white
+         
+        #Return [white, black, total]
+        return [white, black, total]    
     else:
         #returns if file Does not exist in directory
         return False
     
 
+'''
+fileName1 = input('fileName1: ')
+fileName2 = input('fileName2: ')
+fileName3 = input('fileName3: ')
 
-#fileName = input('fileName: ')
+print(ratioBWImported(fileName1) + ratioBWImported(fileName2))
+print(ratioBW(fileName3))
 
-#print(ratioBW(fileName))
-
+print(((ratioBWImported(fileName1)[0]+ratioBWImported(fileName2)[0])/ratioBW(fileName3)[0])*100)
+'''
 
 
